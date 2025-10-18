9d6ac60dea4d5a382a19afa8d0fa7220f0bc6309
Junran Yang <frank8418874@gmail.com>
2025-10-17 22:34:42 -0700

    js updated

--- global.js
+++ global.js
@@ -4,3 +4,7 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
+let navLinks = $$("nav a");  // This uses the $$ helper we defined earlier
+console.log(navLinks);
+
+currentLink?.classList.add("current");
